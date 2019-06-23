450e19136f7756f20df79e5abc8322d6c9bf6b80
willanysilva <willany_lp@hotmail.com>
2019-06-22 23:15:58 -0300

    create some tasks

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Task.create(title: 'Checar o e-mail', done: false)
+Task.create(title: 'Finalizar artigo', done: false)
+Task.create(title: 'Estudar Ruby on Rails', done: false)
+Task.create(title: 'Estudar React', done: true)
+Task.create(title: 'Atualizar o perfil no github', done: true)
